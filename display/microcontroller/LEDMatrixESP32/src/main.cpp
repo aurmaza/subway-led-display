467d7b5b3722f178ad64b2f563b887415a088d7e
Alex <alexmurmaza@gmail.com>
2025-09-09 20:24:51 -0400

    Changes for led light text

--- display/microcontroller/LEDMatrixESP32/src/main.cpp
+++ display/microcontroller/LEDMatrixESP32/src/main.cpp
@@ -1,18 +1,25 @@
 #include <WiFi.h>
 #include <Arduino.h>
 #include <HTTPClient.h>
-#include <iostream>
+#define LED_PIN 2
+#include <LiquidCrystal_I2C.h>
 
-#include <cstdlib>
-#include "dotenv.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
  const char *url = "http://192.168.1.132:8000/test";
+
 void setup()
 {
+  loadDotEnv();
   const char *ssid = std::getenv("SSID");
   const char *pass = std::getenv("PASSWORD");
- 
-
   Serial.begin(115200);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  
+  
+  
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -20,10 +27,18 @@ void setup()
     Serial.print(".");
   }
   Serial.println("\nWiFi connected!");
+
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
+  lcd.setCursor(0,0);
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);                  
+  digitalWrite(LED_PIN, LOW);  
+  delay(500);                  
+
   if (WiFi.status() == WL_CONNECTED)
   {
     HTTPClient http;
@@ -33,6 +48,11 @@ void loop()
     {
       String payload = http.getString();
       Serial.println("Got: " + payload);
+      lcd.print(payload);
+      //Test 2nd Row
+      lcd.setCursor(0,1);
+      lcd.print("N-Train  05 mins");
+      
     }
     else
     {
@@ -40,5 +60,5 @@ void loop()
     }
     http.end();
   }
-  delay(5000);
+  delay(1000);
 }
